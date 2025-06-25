1acd283bd02974011df9581b3ccedc3e919b15fc
muser-gm <musergma@gmail.com>
2025-06-24 23:06:17 -0300

    Actualizando notas y ejercicios de la clase

--- main.js
+++ main.js
@@ -1,6 +1,115 @@
-console.log("Hola mundo!") /*como el mostrar*/
+/*
+//console.log("Hola mundo!") //como el mostrar
+//----------------------------------------------
+// Ejercicio: declarar variables y constantes
 
-/*declarar variables*/
-let edad=10;
+let miEdad=35;
+let miCuidad="La Plata"
+let miTelefono=123456789;
+const miNombre="Mario";
+const miApellido="Cruz";
+const nacionalidadArgentina=true;
 
-console.log(edad)
+console.log(miEdad);
+console.log(miCuidad);
+console.log(miTelefono);
+console.log(miNombre);
+console.log(miApellido);
+console.log(nacionalidadArgentina);
+//-----------------------------------------------
+
+//Operaciones aritméticas:
+let a=5;
+let b=6;
+let c=0;
+//suma (luego: resta, multiplicacion, division, modulo '%', potencia '**')
+c=a+b;
+console.log(c);
+//operaciones de asignacion: 
+c=2;
+c=c+1;
+
+c++; //c=c+1
+c+=5; //c=c+5
+
+//operadores lógicos:
+miembroIglesia=true;
+poseedorSacerdocio=false;
+//and
+let respuesta=miembroIglesia&&poseedorSacerdocio;
+console.log(respuesta);
+//or
+respuesta=miembroIglesia||poseedorSacerdocio;
+console.log(respuesta);
+//not
+respuesta=!miembroIglesia;
+console.log(respuesta);
+
+//EJERCICIO: crear 2 variables booleanas evaluadas por los conectores and, or y not
+climaFrio=true;
+llueve=false;
+
+meQuedoEnCasa=climaFrio && !llueve;
+console.log(meQuedoEnCasa);
+
+meQuedoEnCasa= climaFrio || llueve;
+console.log(meQuedoEnCasa);
+
+//---------------------------------------------
+//OPERACIONES DE COMPARACION SIEMPRE DEVUELVE UN BOOLEANO
+//----------------------------------------------- 
+
+let aa =2;
+let bb = "2";
+
+respuesta = aa == bb; // TRUE: comparacion que solo evalua el valor (contenido) NO EL TIPO
+
+respuesta = aa ===bb; //FALSE: si evalua el tipo.
+
+// > , <, >= , <= ... evalua el contenido sin importar el tipo.
+
+//CONDICIONALES
+let loQueSea = true;
+if (loQueSea=true) {
+    //lo que se va a ejecutar si la condicion es verdadera
+} else {
+    //todo si la condicion no cumple
+}
+
+//ejercicio
+
+edad=4;
+
+if (edad>=8) {
+    console.log("puedes bautizarte")
+    if (edad >=12) {
+        console.log("luego puedes obtener una recomendacion para el templo por medio de una entrevista con tu obispo")
+    }
+}else {
+    console.log("aun no puedes bautizarte, te falta :", 8-edad, "años. Pero si puedes recibir una bendicion especial del sacerdocio!!! :D ")
+}
+*/
+
+
+/*----------------------------------------------------
+EJERCICIO PRUEBA TECNICA:
+Evaluar la edad de una persona y determinar si es de la primaria
+edad de 0 a 11 años
+hombre joven de 12 a 17 años
+jas de 18 a 35 años
+y poseedor de sacerdocio de 36 años en adelante
+--------------------------------------------------------*/
+
+let edad = 31000;
+
+if (edad <= 11) {
+    console.log("Perteneces a la primaria");
+} else if (edad >= 12 && edad <= 17) {
+    console.log("Perteneces a la juventud");
+} else if (edad >= 18 && edad <= 35) {
+    console.log("Eres JAS");
+} else if ( edad >=36 && edad <= 110){
+    console.log("Eres de los mayores y poderosos!");
+} else {
+    console.log("Tu edad ingresada '",edad,"' parece ser incorrecta")
+}
